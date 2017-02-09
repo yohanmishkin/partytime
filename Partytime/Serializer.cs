64ec723dbb5d2382e44aecbd4d02c4420f411f80
Yohan Mishkin <yohanmishkin@protonmail.com>
2017-02-09 12:18:31 -0500

    First passing test

---
+++ Partytime/Serializer.cs
@@ -0,0 +1,22 @@
+﻿using Jil;
+using System;
+using System.IO;
+
+namespace Partytime
+{
+    public class Serializer
+    {
+        public Serializer()
+        {
+        }
+
+        public string Serialize(object data)
+        {
+            using (var output = new StringWriter())
+            {
+                JSON.Serialize(data, output);
+                return output.ToString();
+            }
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,19 @@
+﻿using Partytime;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact(DisplayName = "Serialize simple object")]
+        public void SerializeObject()
+        {
+            var serializer = new Serializer();
+            var simpleObject = new Simple();
+
+            var json = serializer.Serialize(simpleObject);
+
+            Assert.NotNull(json);
+        }
+    }
+}
